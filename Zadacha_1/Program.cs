011a0a08eb63eb21fcb504c8769a2d126fdf3f7b
Kristina Shabovich <Kristina.beloshapko@mail,ru>
2023-12-10 01:45:08 +0300

    add Zadacha 1

---
+++ Zadacha_1/Program.cs
@@ -0,0 +1,33 @@
+﻿// Zadacha 1
+
+ Console.WriteLine("Введите массив");
+    char[,] arr = new char[2,3];
+    for (int i = 0 ; i < arr.GetLength(0) ; i++)
+    {
+        for (int j = 0 ; j < arr.GetLength(1) ; j++)
+        {
+            Console.Write("Введите элемент " +(i+1)+ " строки " + (j+1) + " столбца " + " массива : ");
+            arr[i,j] = Convert.ToChar(Console.ReadLine());
+        }
+    }
+    Console.WriteLine();
+    Console.WriteLine("Массив");
+    for (int i = 0 ; i < arr.GetLength(0) ; i++)
+    {
+        for (int j = 0 ; j < arr.GetLength(1) ; j++)
+        {
+            Console.Write(arr[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+    string str = "";
+    for (int i = 0 ; i < arr.GetLength(0) ; i++)
+    {
+        for (int j = 0 ; j < arr.GetLength(1) ; j++)
+        {
+            str += arr[i,j];
+        }
+    }
+    Console.WriteLine();
+    Console.WriteLine("Строка");
+    Console.WriteLine(str);
