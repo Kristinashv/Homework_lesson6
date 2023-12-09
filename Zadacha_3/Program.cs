9507c1fd1f32ca310640cf31f1b725fcd726fbe1
Kristina Shabovich <Kristina.beloshapko@mail,ru>
2023-12-10 02:31:10 +0300

    add Zadacha 3

---
+++ Zadacha_3/Program.cs
@@ -0,0 +1,25 @@
+﻿// Zadacha 3
+
+Console.WriteLine("Введите строку");
+    string str = Console.ReadLine();
+    string str1 = "";
+    for (int i = 0 ; i < str.Length ; i++)
+    {
+        str1 += str[str.Length-1-i];
+    }
+    int kol = 0;
+    for (int i = 0 ; i < str.Length ; i++)
+    {
+        if (str[i] == str1[i])
+        {
+            kol++;
+        }
+    }
+    if (kol == str.Length)
+    {
+        Console.WriteLine("строка является палиндромом");
+    }
+    else
+    {
+        Console.WriteLine("строка не является палиндромом");
+    }
