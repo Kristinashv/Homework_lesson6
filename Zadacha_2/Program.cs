7cf87e1acaf775d22cb835c0fc300230b14a92e9
Kristina Shabovich <Kristina.beloshapko@mail,ru>
2023-12-10 02:13:07 +0300

    add Zadacha 2

---
+++ Zadacha_2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Zadacha 2
+
+Console.WriteLine("Введите строку");
+    string str = Console.ReadLine();
+    string []str1 = new string [str.Length];
+    for (int i = 0 ; i < str.Length ; i ++)
+    {
+        str1[i] = Convert.ToString(str[i]);
+    }
+    for (int i = 0 ; i < str.Length ; i ++)
+    {
+        if (str[i] == str.ToUpper()[i])
+        {
+            str1[i] = Convert.ToString(str.ToLower()[i]);
+        }
+    }
+    str = "";
+    for (int i = 0 ; i < str1.Length ; i ++)
+    {
+        str += str1[i];
+    }
+    Console.WriteLine("Измененная строка : " + str);
